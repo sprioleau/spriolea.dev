ce21eaf23376526f9ad3c0f6fccfb76bc3bfa8e3
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-12-16 10:47:19 -0500

    Update InfoRails component and styling for same

--- components/InfoRails/index.js
+++ components/InfoRails/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { FiGithub } from "react-icons/fi";
+import { HiOutlineCode } from "react-icons/hi";
 
 const InfoRails = () => {
 	return (
@@ -7,7 +8,10 @@ const InfoRails = () => {
 			<div className="info-rail left">
 				<ul className="info-rail__link-list">
 					<li className="info-rail__link-list-item">
-						<a aria-label="link" href="https://github.com/sprioleau" rel="noreferrer" className="info-rail__link" target="_blank"><FiGithub/></a>
+						<a aria-label="link" href="https://github.com/sprioleau" rel="noreferrer" className="info-rail__link" target="_blank"><span className="icon"><FiGithub /></span></a>
+					</li>
+					<li className="info-rail__link-list-item">
+						<a aria-label="link" href="https://vscode.dev/github/sprioleau/sprioleau.dev" rel="noreferrer" className="info-rail__link" target="_blank"><span className="icon"><HiOutlineCode /></span></a>
 					</li>
 				</ul>
 				<div className="info-rail__accent-line" />
