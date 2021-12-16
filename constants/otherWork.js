af674b6b882c983a2e8bcef883048d8a3a056036
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-12-16 16:42:53 -0500

    Update SelectedWork component and style same

---
+++ constants/otherWork.js
@@ -0,0 +1,59 @@
+const otherWork = [
+  {
+    title: "Structured Style", 
+    year: "2021",
+    builtFor: "CS52 Course",
+    tags: ["HTML", "CSS", "Adobe Dimension"],
+    links: {
+      gitHubUrl: "https://github.com/dartmouth-cs52-21S/lab1-landingpage-sprioleau",
+      sourceCodeUrl: "https://vscode.dev/github/dartmouth-cs52-21S/lab1-landingpage-sprioleau",
+      deployedUrl: "https://structured-style.netlify.app/",
+    }
+  },
+  {
+    title: "Quizzd", 
+    year: "2021",
+    builtFor: "CS52 Course",
+    tags: ["HTML", "CSS", "JS", "Adobe Dimension"],
+    links: {
+      gitHubUrl: "https://github.com/dartmouth-cs52-21S/lab2-sprioleau",
+      sourceCodeUrl: "https://vscode.dev/github/dartmouth-cs52-21S/lab2-sprioleau",
+      deployedUrl: "https://quizzd.netlify.app/",
+    }
+  },
+  {
+    title: "Videwhoa", 
+    year: "2021",
+    builtFor: "CS52 Course",
+    tags: ["React", "Redux", "YouTube API", "Sass"],
+    links: {
+      gitHubUrl: "https://github.com/dartmouth-cs52-21S/react-intro-sprioleau",
+      sourceCodeUrl: "https://vscode.dev/github/dartmouth-cs52-21S/react-intro-sprioleau",
+      deployedUrl: "https://videwhoa.netlify.app/",
+    }
+  },
+  {
+    title: "PokéMe", 
+    year: "2021",
+    builtFor: "CS52 Course",
+    tags: ["React", "Redux", "Sass", "Mongo DB", "Express"],
+    links: {
+      gitHubUrl: "https://github.com/dartmouth-cs52-21S/platform-client-sprioleau",
+      sourceCodeUrl: "https://vscode.dev/github/dartmouth-cs52-21S/platform-client-sprioleau",
+      deployedUrl: "https://pokeme-cards.netlify.app/",
+    }
+  },
+  {
+    title: "C19 Inside Scoop", 
+    year: "2019",
+    builtFor: "Client",
+    tags: ["Webflow", "CSS"],
+    links: {
+      gitHubUrl: "",
+      sourceCodeUrl: "",
+      deployedUrl: "https://www.c19insider.com/",
+    }
+  },
+]
+
+export default otherWork;
