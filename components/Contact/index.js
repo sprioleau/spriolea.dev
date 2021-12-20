7a76fb733ed4272a114e56eb6288e1f636cc0ce7
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-12-20 15:24:32 -0500

    Update Contact section, use library for copying email to clipboard

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -1,15 +1,23 @@
 
 import { MdOutlineMarkEmailRead } from "react-icons/md"
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { toast } from "react-toastify";
 import PageSectionLayout from "../PageSectionLayout";
 
 const Contact = () => {
+	const EMAIL_ADDRESS = "sq.prioleau@gmail.com";
+
+	const handleToast = () => toast.dark("Email address successfully copied!", { toastId: "email" });
+	
   return (
 		<PageSectionLayout sectionId="contact" sectionTitle="Get in touch" center offset="0px">
-			<a href="mailto:sq.prioleau@gmail.com" className="contact__card">
-				<span className="icon mail-icon"><MdOutlineMarkEmailRead /></span>
-				<h3 className="contact__card-heading">You can find me at</h3>
-				<p className="a contact__card-email">sq.prioleau@gmail.com</p>
-			</a>
+			<CopyToClipboard text={EMAIL_ADDRESS} onCopy={handleToast}>
+				<div className="contact__card">
+					<span className="icon mail-icon"><MdOutlineMarkEmailRead /></span>
+					<h3 className="contact__card-heading">You can find me at</h3>
+						<p className="a contact__card-email">{EMAIL_ADDRESS}</p>
+				</div>
+			</CopyToClipboard>
 		</PageSectionLayout>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import "../styles/styles.scss";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MyApp = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />
+			<ToastContainer
+				position="top-center"
+				autoClose={3000}
+			/>
+		</>
+	);
 };
 
 export default MyApp;
