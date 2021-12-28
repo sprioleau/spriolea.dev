a3a8603995e0a1cbd8405ff491e930c09825fbbe
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-12-28 12:22:24 -0500

    :pencil: Minor formatting tweaks

--- constants/navLinks.js
+++ constants/navLinks.js
@@ -1,9 +1,9 @@
 const navLinks = [
-  {label: "About", link: "#about"},
-  {label: "Experience", link: "#experience"},
-  {label: "Work", link: "#work"},
-  {label: "Skills", link: "#skills"},
-  {label: "Contact", link: "#contact"},
+  { label: "About", link: "#about" },
+  { label: "Experience", link: "#experience" },
+  { label: "Work", link: "#work" },
+  { label: "Skills", link: "#skills" },
+  { label: "Contact", link: "#contact" },
 ]
   
 export default navLinks;
