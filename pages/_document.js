aef2079e3ad4522b72a75e9a2eb08764fe466d7a
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-12-29 13:15:21 -0500

    Prepare and reference social card, update metadata

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,19 @@ class MyDocument extends Document {
 					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 					<link rel="icon" href="/favicon.png" />
 					<meta name="description" content="San'Quan Prioleau is a frontend web developer with an eye for good design, who loves combining ideas to create new things." />
+					<meta itemProp="name" content="San'Quan Prioleau" />
+					<meta itemProp="description" content="San'Quan Prioleau is a frontend web developer with an eye for good design, who loves combining ideas to create new things." />
+					<meta itemProp="image" content="http://sprioleau.dev/images/sprioleau-social-card.png" />
+					<meta property="og:url" content="http://sprioleau.dev" />
+					<meta property="og:type" content="website" />
+					<meta property="og:title" content="San'Quan Prioleau" />
+					<meta property="og:description" content="San'Quan Prioleau is a frontend web developer with an eye for good design, who loves combining ideas to create new things." />
+					<meta property="og:image" content="http://sprioleau.dev/images/sprioleau-social-card.png" />
+					<meta name="twitter:card" content="summary_large_image" />
+					<meta name="twitter:title" content="San'Quan Prioleau" />
+					<meta name="twitter:description" content="San'Quan Prioleau is a frontend web developer with an eye for good design, who loves combining ideas to create new things." />
+					<meta name="twitter:image" content="http://sprioleau.dev/images/sprioleau-social-card.png" />
+					<meta name="description" content="San'Quan Prioleau is a frontend web developer with an eye for good design, who loves combining ideas to create new things." />
 					<link rel="stylesheet" href="https://use.typekit.net/hnv6eep.css" />
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
