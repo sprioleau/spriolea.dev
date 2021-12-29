8490e3af0f79af8bda27c5d2a3caa9fb05a55284
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-12-29 13:27:07 -0500

    Copy email address instead of using mailto link

--- components/InfoRails/index.js
+++ components/InfoRails/index.js
@@ -1,6 +1,8 @@
 
 import { FiGithub } from "react-icons/fi";
 import { HiOutlineCode } from "react-icons/hi";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { copyNotification } from "../../utils";
 
 const InfoRails = ({ siteDetails }) => {
 	const { creator: { email, links: { gitHubUrl } }, meta: { vsCodeUrl } } = siteDetails[0];
@@ -21,7 +23,9 @@ const InfoRails = ({ siteDetails }) => {
 			<div className="info-rail right">
 				<ul className="info-rail__link-list">
 					<li className="info-rail__link-list-item">
-						<a aria-label="link" href={`mailto:${email}`} className="info-rail__link text-link">{email}</a>
+						<CopyToClipboard text={email} onCopy={copyNotification}>
+							<span className="info-rail__link text-link">{email}</span>
+						</CopyToClipboard>
 					</li>
 				</ul>
 				<div className="info-rail__accent-line" />
