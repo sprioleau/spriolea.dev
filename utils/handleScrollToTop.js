09c29f82cc2bc382dffb9882c29fa2a5db924a48
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-12 13:56:28 -0500

    Scroll to top on logo click

---
+++ utils/handleScrollToTop.js
@@ -0,0 +1,11 @@
+const handleScrollToTop = (e, router) => {
+  e.preventDefault();
+
+  router.push("/");
+
+  if (window !== undefined) {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+}
+
+export default handleScrollToTop;
