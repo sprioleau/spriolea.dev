1ecb9f3e5a14e2338cb0149acecf7535a80730e2
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-14 01:05:13 -0500

    Move warning to top of file

--- utils/organizeByKey.js
+++ utils/organizeByKey.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-param-reassign */
+
 import getKeyValue from "./getKeyValue";
 
 const organizeByKey = (array, path) => array.reduce((result, item) => {
   const property = getKeyValue(item, path)
 
   if (!result[property]) {
-    // eslint-disable-next-line no-param-reassign
     result[property] = [item];
   } else {
     result[property].push(item)
