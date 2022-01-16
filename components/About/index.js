c7b753ac6f555d586a9e647a73b2e663549c7a92
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-15 19:17:51 -0500

    :fire: Remove unused code

--- components/About/index.js
+++ components/About/index.js
@@ -15,14 +15,6 @@ const About = ({ content }) => {
 						<p key={_key}><PortableTextBlock childrenContent={children} markDefs={markDefs}/></p>
 					))}
 				</div>
-				{/* <div className="about__tech">
-					<h3 className="about__tech-intro">Technologies I enjoy</h3>
-					<ul className="about__tech-list">
-						{technologiesList.map((technology) => (
-							<li key={ technology} className="about__tech-list-item"><p>{technology}</p></li>
-							))}
-					</ul>
-				</div> */}
 			</div>
 			<div className="about__image">
 				<Image
