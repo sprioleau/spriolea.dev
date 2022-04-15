f53a78c4ca5675671cb10c44c178e83e828d0f57
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-04-14 23:31:02 -0400

    ⚰️ Remove unused store code

--- store/index.js
+++
@@ -1,8 +0,0 @@
-import create from "zustand";
-
-const useStore = create((set) => ({
-  data: null,
-  updateData: (dataFromApi) => set((state) => ({ data: dataFromApi })),
-}));
-
-export default useStore;
