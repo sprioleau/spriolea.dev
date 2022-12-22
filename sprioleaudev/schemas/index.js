a52a094d201a005d929e41b4145b1dc651dec8b2
San'Quan Prioleau <sq.prioleau@gmail.com>
2022-12-21 23:54:24 -0500

    ⬆️ Upgrade sanity studio

---
+++ sprioleaudev/schemas/index.js
@@ -0,0 +1,42 @@
+// eslint-disable-next-line import/prefer-default-export
+import about from "./about";
+import association from "./association";
+import blockContent from "./blockContent";
+import contact from "./contact";
+import category from "./category";
+import credit from "./credit";
+import employer from "./employer";
+import experience from "./experience";
+import footer from "./footer";
+import hero from "./hero";
+import internalProject from "./internalProject";
+import jobType from "./jobType";
+import kudos from "./kudos";
+import navLink from "./navLink";
+import project from "./project";
+import siteDetails from "./siteDetails";
+import skill from "./skill";
+import tag from "./tag";
+import technology from "./technology";
+
+export default [
+  about,
+  association,
+  blockContent,
+  contact,
+  category,
+  credit,
+  employer,
+  experience,
+  footer,
+  hero,
+  internalProject,
+  jobType,
+  kudos,
+  navLink,
+  project,
+  siteDetails,
+  skill,
+  tag,
+  technology,
+];
