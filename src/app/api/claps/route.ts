46b4e51c2b17e0f51db870664808e4294c2b9c26
San'Quan Prioleau <49278940+sprioleau@users.noreply.github.com>
2023-05-16 05:46:10 -0400

    Update route.ts

--- src/app/api/claps/route.ts
+++ src/app/api/claps/route.ts
@@ -1,8 +1,6 @@
 import { get } from "@vercel/edge-config";
 
-export const config = {
-  runtime: "edge",
-};
+export const runtime = "edge";
 
 export async function GET(request: Request) {
   const claps = await get("claps");
