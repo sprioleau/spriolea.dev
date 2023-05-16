48d464b8f819687dd2c3de923fa5f1e0ab0567b6
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-05-15 22:40:06 -0400

    🧹 Clean up repo

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,3 @@
+export async function GET(request: Request) {
+  return new Response(JSON.stringify({ message: "hello" }, null, 2));
+}
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,18 +4,9 @@ import { client, queries } from "@/libs/sanity";
 import { FooterContent } from "@/components";
 import { FooterData } from "@/schemas/types";
 
-// const url = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/get-contributions`;
-
-// console.log("🚀 ~ file: index.tsx:10 ~ url:", url);
-
 export default async function Footer() {
   const footerContent = await client.fetch<FooterData[]>(queries.footer);
-  const { contributions = 10 } =
-    (await fetchContributions()) as ContributionsData;
-  console.log(
-    "🚀 ~ file: index.tsx:14 ~ Footer ~ contributions:",
-    contributions
-  );
+  const { contributions } = (await fetchContributions()) as ContributionsData;
 
   const { body } = footerContent[0];
 
