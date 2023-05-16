71eb831e904f0e9e3bde73f68c60beeda1352823
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-05-15 22:36:06 -0400

    🔥 Remove API function

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,18 +1,21 @@
+import { ContributionsData, fetchContributions } from "@/libs/github";
 import { client, queries } from "@/libs/sanity";
 
-import { ContributionsData } from "@/libs/github";
 import { FooterContent } from "@/components";
 import { FooterData } from "@/schemas/types";
 
-const url = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/get-contributions`;
+// const url = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/get-contributions`;
 
-console.log("🚀 ~ file: index.tsx:10 ~ url:", url);
+// console.log("🚀 ~ file: index.tsx:10 ~ url:", url);
 
 export default async function Footer() {
   const footerContent = await client.fetch<FooterData[]>(queries.footer);
-  const { contributions = 10 } = (await fetch(url)
-    .then((res) => res.json())
-    .catch((reason) => console.error(reason))) as ContributionsData;
+  const { contributions = 10 } =
+    (await fetchContributions()) as ContributionsData;
+  console.log(
+    "🚀 ~ file: index.tsx:14 ~ Footer ~ contributions:",
+    contributions
+  );
 
   const { body } = footerContent[0];
 
