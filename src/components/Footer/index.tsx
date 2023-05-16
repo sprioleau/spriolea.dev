a4d51b3b4f8ea8b15a22e04dd4c25bcc619d14cb
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-05-15 22:23:05 -0400

    🔊 Add log statements

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,11 +5,17 @@ import { ContributionsData } from "@/libs/github";
 import { FooterContent } from "@/components";
 import { FooterData } from "@/schemas/types";
 
+const url = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/get-contributions`;
+
+console.log("🚀 ~ file: index.tsx:10 ~ url:", url);
+
 export default async function Footer() {
   const footerContent = await client.fetch<FooterData[]>(queries.footer);
-  const { contributions } = (await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/get-contributions`
-  ).then((res) => res.json())) as ContributionsData;
+  const { contributions } = (await fetch(url).then((res) =>
+    res.json()
+  )) as ContributionsData;
+
+  console.log("🚀 ~ file: index.tsx:14 ~ Footer ~ url:", url);
 
   const { body } = footerContent[0];
 
