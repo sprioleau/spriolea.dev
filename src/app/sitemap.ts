5161029593bb4ec568909a11507172a27a30b7df
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-05-18 11:08:10 -0400

    🔍 Update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,21 +6,21 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://sprioleau.dev",
       lastModified: new Date(),
     },
-    {
-      url: "https://sprioleau.dev/about",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://sprioleau.dev/experience",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://sprioleau.dev/skills",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://sprioleau.dev/work",
-      lastModified: new Date(),
-    },
+    // {
+    //   url: "https://sprioleau.dev/about",
+    //   lastModified: new Date(),
+    // },
+    // {
+    //   url: "https://sprioleau.dev/experience",
+    //   lastModified: new Date(),
+    // },
+    // {
+    //   url: "https://sprioleau.dev/skills",
+    //   lastModified: new Date(),
+    // },
+    // {
+    //   url: "https://sprioleau.dev/work",
+    //   lastModified: new Date(),
+    // },
   ];
 }
