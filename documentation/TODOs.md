ea34485f0e35e3519dffb1ee662ee418a1f69ab1
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-05-21 18:54:02 -0400

    ✨ Add spotlight to contact card

--- documentation/TODOs.md
+++ documentation/TODOs.md
@@ -10,6 +10,7 @@
 - [x] Use Vercel Edge Config for clap button
 - [x] Create custom clap button
 - [x] Use app directory APIs (`route.ts` files)
+- [x] Add [spotlight animation](https://buildui.com/recipes/spotlight) to contact card
 - [ ] Add metadata
 - [ ] Remove unused packages
 - [ ] Add toast message
--- src/components/Contact/ContactCard.tsx
+++ src/components/Contact/ContactCard.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
+
 import { ContactData } from "@/schemas/types";
 import { handleCopyEmailToClipboard } from "@/utils";
 import icons from "../Icons";
@@ -9,6 +11,20 @@ type Props = {
 };
 
 export default function ContactCard({ contact: { email, overline } }: Props) {
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  function handleMouseMove({
+    currentTarget,
+    clientX,
+    clientY,
+  }: React.MouseEvent<HTMLDivElement>) {
+    const { left, top } = currentTarget.getBoundingClientRect();
+
+    mouseX.set(clientX - left);
+    mouseY.set(clientY - top);
+  }
+
   const getFormattedEmail = (email: string) => {
     const [userName, serverDomain] = email.split("@");
     return (
@@ -25,7 +41,20 @@ export default function ContactCard({ contact: { email, overline } }: Props) {
       role="button"
       tabIndex={0}
       onClick={() => handleCopyEmailToClipboard(email)}
+      onMouseMove={handleMouseMove}
     >
+      <motion.div
+        className="contact__spotlight"
+        style={{
+          background: useMotionTemplate`
+            radial-gradient(
+              500px circle at ${mouseX}px ${mouseY}px,
+              rgba(255, 196, 0, 0.1),
+              transparent 80%
+            )
+          `,
+        }}
+      />
       <span className="icon mail-icon">{icons.email}</span>
       <h3 className="contact__card-heading">{overline}</h3>
       <p className="a contact__card-email">{getFormattedEmail(email)}</p>
