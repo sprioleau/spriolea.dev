cdc6a16d7cf7339135c620700484ec75914bc194
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-05-22 12:16:44 -0400

    ♻️ Simplify return statement

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -9,19 +9,40 @@ import {
 } from "@/components";
 
 import { DefaultMetaTags } from "@/meta";
-import type { Metadata } from "next";
 
-const title = "San'Quan Prioleau - Frontend Engineer";
-const description = "Personal website of San'Quan Prioleau";
-
-export const metadata: Metadata = {
-  title,
-  description,
-  openGraph: {
-    description,
-    title,
-  },
+type PageSection = {
+  id: string;
+  title: string;
+  component: () => Promise<JSX.Element>;
 };
+
+const pageSections: PageSection[] = [
+  {
+    id: "about",
+    title: "My Story",
+    component: About,
+  },
+  {
+    id: "experience",
+    title: "Experience",
+    component: Experience,
+  },
+  {
+    id: "work",
+    title: "Selected Work",
+    component: Work,
+  },
+  {
+    id: "skills",
+    title: "Relevant Skills",
+    component: Skills,
+  },
+  {
+    id: "contact",
+    title: "Get in Touch",
+    component: Contact,
+  },
+];
 
 export default async function Home() {
   return (
@@ -29,41 +50,16 @@ export default async function Home() {
       <DefaultMetaTags />
       {/* @ts-expect-error Async Server Component */}
       <Hero />
-      <PageSectionLayout
-        sectionId="about"
-        sectionTitle="My Story"
-      >
-        {/* @ts-expect-error Async Server Component */}
-        <About />
-      </PageSectionLayout>
-      <PageSectionLayout
-        sectionId="experience"
-        sectionTitle="Experience"
-      >
-        {/* @ts-expect-error Async Server Component */}
-        <Experience />
-      </PageSectionLayout>
-      <PageSectionLayout
-        sectionId="work"
-        sectionTitle="Selected Work"
-      >
-        {/* @ts-expect-error Async Server Component */}
-        <Work />
-      </PageSectionLayout>
-      <PageSectionLayout
-        sectionId="skills"
-        sectionTitle="Relevant Skills"
-      >
-        {/* @ts-expect-error Async Server Component */}
-        <Skills />
-      </PageSectionLayout>
-      <PageSectionLayout
-        sectionId="contact"
-        sectionTitle="Get In Touch"
-      >
-        {/* @ts-expect-error Async Server Component */}
-        <Contact />
-      </PageSectionLayout>
+      {pageSections.map(({ id, title, component: Component }) => (
+        <PageSectionLayout
+          key={id}
+          sectionId={id}
+          sectionTitle={title}
+        >
+          {/* @ts-expect-error Async Server Component */}
+          <Component />
+        </PageSectionLayout>
+      ))}
     </>
   );
 }
