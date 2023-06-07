407f8ee9c001d89202052f63ef97738f83a59d53
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-06-06 22:47:07 -0400

    ♻️ Use HTML anchor tag

--- src/components/CallToActionButton/index.tsx
+++ src/components/CallToActionButton/index.tsx
@@ -1,8 +1,4 @@
-"use client";
-
 import { CallToAction } from "@/schemas/types";
-import Link from "next/link";
-// import { useRouter } from "next/navigation";
 
 type Props = {
   cta: CallToAction;
@@ -10,20 +6,12 @@ type Props = {
 };
 
 export default function CallToActionButton({ cta, className }: Props) {
-  // const router = useRouter();
-
-  // function handleClick() {
-  //   router.push(`#${cta.linkTarget}`);
-  // }
-
   return (
-    <Link
-      href={`/#${cta.linkTarget}`}
-      type="button"
+    <a
+      href={`#${cta.linkTarget}`}
       className={`${className} button`}
-      // onClick={handleClick}
     >
       {cta.label}
-    </Link>
+    </a>
   );
 }
