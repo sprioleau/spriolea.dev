7c8990b9aa4a4d9bdb3c2c2e77a4ee6c7107389c
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-08-04 10:04:34 -0400

    📝 Update experience

--- utils/getKeyValue.js
+++ utils/getKeyValue.js
@@ -1,5 +1,3 @@
-const getKeyValue = (object, path) => {
-  return path.split(".").reduce((obj, key) => obj[key], object);
-};
+const getKeyValue = (object, path) => path.split(".").reduce((obj, key) => obj[key], object);
 
 export default getKeyValue;
