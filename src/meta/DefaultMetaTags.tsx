9fa0ddf3faa7744c2ff28f3df6fc8d11d305c3f1
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-09-15 09:56:59 -0400

    🔥 Remove unused files

--- src/meta/DefaultMetaTags.tsx
+++
@@ -1,8 +0,0 @@
-export default function DefaultMetaTags() {
-	return (
-		<>
-			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<link href="/favicon.png" rel="shortcut icon" type="image/png" />
-		</>
-	);
-}
