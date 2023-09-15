f3f9d7e050aefb34c18830bc9de64998195e7fc2
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-09-15 10:00:34 -0400

    🔥 Keep meta tags in layout component

--- src/app/projects/all/page.tsx
+++ src/app/projects/all/page.tsx
@@ -1,17 +1,14 @@
 import { BackButton, Footer, ProjectTable } from "@/components";
-import { client, queries } from "@/libs/sanity";
-
-import { DefaultMetaTags } from "@/meta";
-import { Project } from "@/schemas/types";
 
 import styles from "./styles.module.scss";
+import { client, queries } from "@/libs/sanity";
+import { Project } from "@/schemas/types";
 
 export default async function ProjectsArchive() {
   const projects = await client.fetch<Project[]>(queries.allProjects);
 
   return (
     <>
-      <DefaultMetaTags />
       <div className={styles.page}>
         <BackButton
           to="/"
