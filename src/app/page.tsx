626569d288145d27836b5e76cf18fb32f93188d4
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-09-23 12:51:27 -0400

    🎨 Reformat HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,16 @@
 import { About, Contact, Experience, Footer, Hero, PageSectionLayout, Skills, Spotlight, Work } from "@/components";
 import ToastContainer from "@/components/ToastContainer";
 
-type PageSection = {
-  id: string;
-  title: string;
-  component: () => Promise<JSX.Element>;
-};
-
-const pageSections: PageSection[] = [
-  {
-    id: "about",
-    title: "My Story",
-    component: About,
-  },
-  {
-    id: "experience",
-    title: "Experience",
-    component: Experience,
-  },
-  {
-    id: "work",
-    title: "Selected Work",
-    component: Work,
-  },
-  {
-    id: "skills",
-    title: "Relevant Skills",
-    component: Skills,
-  },
-  {
-    id: "contact",
-    title: "Get in Touch",
-    component: Contact,
-  },
+// prettier-ignore
+const pageSections = [
+  { id: "about"     , title: "My Story"       , component: About      },
+  { id: "experience", title: "Experience"     , component: Experience },
+  { id: "work"      , title: "Selected Work"  , component: Work       },
+  { id: "skills"    , title: "Relevant Skills", component: Skills     },
+  { id: "contact"   , title: "Get in Touch"   , component: Contact    },
 ];
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
       <ToastContainer />
