e2a9f77ecfe50aae4829064c41c13a4b36d21673
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-05-12 15:52:33 -0400

    🔥 Remove kudos section

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,10 +1,10 @@
-import { ExperienceSlider, ProjectKudos } from "@/components";
+import { ExperienceSlider } from "@/components";
 
 export default async function Experience() {
   return (
     <>
       <ExperienceSlider />
-      <ProjectKudos />
+      {/* <ProjectKudos /> */}
     </>
   );
 }
